f074cba03d9f2155ec5020b89e1a043a8f4046cc
Matthew Ennis <55544151+MatthewEnnis@users.noreply.github.com>
2019-10-08 19:37:20 +0100

    file version

--- Week 4/gibberish.py
+++ Week 4/gibberish.py
@@ -12,37 +12,63 @@ def inputcheck(word):
 		word = input("Invalid input, please try again: ")
 	return word
 
+def fileread(filename):
+	while True:
+		try:
+			with open(filename, "r") as file:
+				text = file.read()
+		except FileNotFoundError:
+			filename = input("Invalid file, please try again: ")
+		else:
+			return text
+
+def newfilename(oldfilename):
+	dotspot = -(oldfilename[::-1].find(".")+1)
+	return oldfilename[:dotspot] + "Gibberish" + oldfilename[dotspot:]
+
 cont = "y"
 
 while cont == "y":
-	gib1 = input("Enter your first Gibberish syllable (* for vowel substitute): ")
+	gib1 = input("Enter your first Gibberish syllable (add * for the vowel substitute): ")
 	gib1 = inputcheck(gib1)
 
-	gib2 = input("Enter your second Gibberish syllable (* for vowel substitute): ")
+	gib2 = input("Enter the second Gibberish syllable (* for vowel substitute): ")
 	gib2 = inputcheck(gib2)
+	
+	filename = input("Please enter a file you want to translate:\n--> ")
 
-	word = input("Please enter a word you want to translate: ")
-	word = inputcheck(word)
-
-	newword = ""
+	text = fileread(filename)
+	
+	newtext = ""
 	syllable = gib1
+	previousconsonant = True
 
-	for letter in word:
-		if letter in vowels:
+	for letter in text:
+		if previousconsonant and letter in vowels:
 			for character in syllable:
 				if character == "*":
-					newword += letter
+					newtext += letter
 				else:
-					newword += character
-			newword += letter
+					newtext += character
+			newtext += letter
 			syllable = gib2
+			previousconsonant = False
 		else:
-			newword += letter
-
-	print("Your final word:\n" + newword)
-
+			newtext += letter
+			previousconsonant = True
+			if letter == " ":
+				syllable = gib1
+	
+	outputfilename = newfilename(filename)
+	with open(outputfilename, "w") as outputfile:
+		outputfile.write(newtext)
+	
+	print("Your final text is:\n" + newtext)
+	
+	print("\nResults can be seen in file " + outputfilename)
+	
 	cont = input("Play again? (y/n) ")
 	while cont not in "yn":
 		cont = input("Please enter y to continue or n to quit: ")
 
-print("Thanks for playing!")
+print("\nThanks for playing!")
